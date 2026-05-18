71e92fea38d3d2bc57166fb67077b219e87f5558
Виктория Смирнова <SixDaysAgo@yandex.ru>
2026-05-18 23:18:26 +0300

    Добавление блока для игры

--- game/clickme.py
+++ game/clickme.py
@@ -14,20 +14,33 @@ ITEM_HEIGHT = 30
 file_path = Path("src/data.db")
 if not file_path.is_file():
     db.bd_create()
+# Конец проверки
 
 pygame.init()
 
-# Основной экран
+# Настройки основного экрана
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Игра без названия')
 screen.fill(colors.MAIN_COLOR)
+# Конец настроек
+
+# Блок с игрой
+game_h = WINDOW_HEIGHT-2*PADDING
+game_block_rect = pygame.Rect(PADDING, PADDING, game_h, game_h)
+# The end блок с игрой
 
 run = True
 
+# Основной цикл
 while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
+
+    # Отрисовка блока с игрой
+    pygame.draw.rect(screen, colors.SECONDARY_COLOR, game_block_rect)
+    pygame.draw.rect(screen, colors.BORDER_COLOR, game_block_rect, 3)
+    # Конец отрисовки
 
     pygame.display.flip()
 
