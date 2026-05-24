7e1cc4d41a0500f75d87cd0630255d8c3c2b962d
Виктория Смирнова <SixDaysAgo@yandex.ru>
2026-05-24 22:35:03 +0300

    Замена всех планов (экрна входа)

--- game/clickme.py
+++ game/clickme.py
@@ -6,8 +6,8 @@ import modules.button as button
 from pathlib import Path
 
 # Константы
-WINDOW_HEIGHT = 600
-WINDOW_WIDTH = 950
+WINDOW_HEIGHT = 540
+WINDOW_WIDTH = 1000
 PADDING = 20
 BUTTON_HEIGHT = 50
 ITEM_HEIGHT = 30
@@ -21,30 +21,55 @@ if not file_path.is_file():
 pygame.init()
 
 FONT = pygame.font.Font(None, 32)
+SMALL_FONT = pygame.font.Font(None, 26)
 
 # Настройки основного экрана
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Игра без названия')
 screen.fill(colors.MAIN_COLOR)
-# Конец настроек
-
-# Блок с игрой
-game_h = WINDOW_HEIGHT-2*PADDING
-game_block_rect = pygame.Rect(PADDING, PADDING, game_h, game_h)
-# The end блок с игрой
-
-# Блок с формой входа
-auth_rect = pygame.Rect(PADDING+game_h/6, PADDING+game_h/3, game_h/1.5, game_h/3)
-auth_input = input.Input(PADDING+game_h/6+35, PADDING+game_h/2.2)
-
-auth_button = button.Button("Войти", 40, game_h/3, colors.SECONDARY_COLOR, PADDING+game_h/3, PADDING+game_h/3+game_h/4.5)
-auth_button.create_rect()
-
-text_surface = FONT.render("Введите ник", True, (0, 0, 0))
-# Конец блока с формой входа
 
 run = True
-auth_selected = True
+page = "Auth"
+# Конец настроек
+
+# Блок с формой входа
+auth_width = 480
+auth_height = 240
+
+auth_rect = pygame.Rect((WINDOW_WIDTH-auth_width)/2, (WINDOW_HEIGHT-auth_height-30)/2, auth_width, auth_height)
+auth_surface = pygame.Surface((auth_rect.width, auth_rect.height))
+pygame.draw.rect(auth_surface, colors.MAIN_COLOR, auth_surface.get_rect())
+
+text_surface = FONT.render("Форма входа", True, colors.TEXT_COLOR)
+auth_surface.blit(text_surface, ((auth_rect.width-text_surface.get_width())/2, PADDING))
+
+login_text_surface = SMALL_FONT.render("Логин", True, colors.TEXT_COLOR)
+password_text_surface = SMALL_FONT.render("Пароль", True, colors.TEXT_COLOR)
+
+auth_surface.blit(login_text_surface, (PADDING*2, 85))
+auth_surface.blit(password_text_surface, (PADDING*2, input.INPUT_HEIGHT+105))
+
+login_input = input.Input()
+password_input = input.Input(True)
+
+def on_login():
+    if login_input.value == test_login and password_input.value == test_password:
+        change_page("Game")
+
+def change_page(page_name):
+    global page
+    page = page_name
+
+sing_in_button = button.Button("Войти", 50, 120, on_login)
+sing_in_button.create_rect(270, 120+input.INPUT_HEIGHT*2)
+
+registration_button = button.Button("У меня нет аккаунта", 50, 200, callback=lambda: change_page("Registration"))
+registration_button.create_rect(50, 120+input.INPUT_HEIGHT*2)
+
+# Конец блока
+
+test_login = "1234"
+test_password = "4321q"
 
 # Основной цикл
 while run:
@@ -52,40 +77,28 @@ while run:
         if e.type == pygame.QUIT:
             run = False
 
-        if e.type == pygame.MOUSEBUTTONDOWN:
-            if auth_button.rect.collidepoint(e.pos):
-                if len(auth_input.value) > 3:
-                    auth_selected = False
-
-            auth_input.active = True if auth_input.rect.collidepoint(e.pos) else False
-
-        if e.type == pygame.KEYDOWN:
-            if auth_input.active:
-                if e.key == pygame.K_BACKSPACE:
-                    auth_input.value = auth_input.value[:-1]
-                else:
-                    if len(auth_input.value) < 10:
-                        auth_input.value += e.unicode
-
-
-    mouse_pos = pygame.mouse.get_pos()
-
-    # Отрисовка блока с игрой
-    pygame.draw.rect(screen, colors.SECONDARY_COLOR, game_block_rect)
-    pygame.draw.rect(screen, colors.BORDER_COLOR, game_block_rect, 3)
-    # Конец отрисовки
+        if page == "Auth":
+            login_input.handle_event(e, (auth_rect.x, auth_rect.y))
+            password_input.handle_event(e, (auth_rect.x, auth_rect.y))
+            sing_in_button.handle_event(e,(auth_rect.x, auth_rect.y))
+            registration_button.handle_event(e,(auth_rect.x, auth_rect.y))
 
     # Отрисовка блока входа
-    if auth_selected:
-        pygame.draw.rect(screen, colors.MAIN_COLOR, auth_rect)
-        pygame.draw.rect(screen, colors.BORDER_COLOR, auth_rect, 3)
+    if page == "Auth":
+        screen.fill(colors.SECONDARY_COLOR)
+        screen.blit(auth_surface, auth_rect.topleft)
 
-        auth_button.color = colors.BORDER_COLOR if auth_button.rect.collidepoint(mouse_pos) else colors.SECONDARY_COLOR
-        auth_input.draw_input(screen)
-        auth_button.draw_button(screen)
+        login_input.draw_input(auth_surface, 150,80)
+        password_input.draw_input(auth_surface, 150,100+input.INPUT_HEIGHT)
 
-        screen.blit(text_surface, (PADDING+game_h/3+15, PADDING+game_h/2.5-10))
-    # Конец отрисовки
+        sing_in_button.draw_button(auth_surface)
+        registration_button.draw_button(auth_surface)
+
+    elif page == "Game":
+        screen.fill(colors.MAIN_COLOR)
+
+    elif page == "Registration":
+        screen.fill(colors.MAIN_COLOR)
 
     pygame.display.flip()
 
--- game/modules/input.py
+++ game/modules/input.py
@@ -3,23 +3,48 @@ import pygame
 INPUT_HEIGHT = 30
 INPUT_WIDTH = 300
 
-
 class Input:
-    def __init__(self, x, y):
+    def __init__(self, is_password = False):
         self.value = ""
         self.rect = None
-        self.x = x
-        self.y = y
         self.active = False
+        self.is_password = is_password
 
-    def draw_input(self, block):
+    def draw_input(self, block, x, y):
         FONT = pygame.font.Font(None, 32)
-        self.rect = pygame.Rect(self.x, self.y, INPUT_WIDTH, INPUT_HEIGHT)
+        self.rect = pygame.Rect(x, y, INPUT_WIDTH, INPUT_HEIGHT)
 
         pygame.draw.rect(block, (200,200,200), self.rect)
 
         color = (0,0,0) if self.active else (100,100,100)
+
+        text_surface = FONT.render("*" * len(self.value)  if self.is_password else self.value, True, (0, 0, 0))
+
+        block.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
         pygame.draw.rect(block, color, self.rect, 2)
 
-        text_surface = FONT.render(self.value, True, (0, 0, 0))
-        block.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
+    def handle_event(self, event, surface_rect=None):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if surface_rect:
+                mouse_x = event.pos[0] - surface_rect[0]
+                mouse_y = event.pos[1] - surface_rect[1]
+                mouse_pos = (mouse_x, mouse_y)
+            else:
+                mouse_pos = event.pos
+
+            if self.rect and self.rect.collidepoint(mouse_pos):
+                self.active = True
+                return True
+            else:
+                self.active = False
+
+        if event.type == pygame.KEYDOWN and self.active:
+            if event.key == pygame.K_BACKSPACE:
+                self.value = self.value[:-1]
+            elif event.key == pygame.K_RETURN:
+                self.active = False
+                return True
+            else:
+                if len(self.value) < 10:
+                    self.value += event.unicode
+        return False
